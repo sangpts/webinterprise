457e789ccbe2ec342a3cf6e3ae83c03e0e3084ba
sangpts <sangphungsh2000@gmail.com>
2022-04-18 10:48:31 +0700

    commit

---
+++ TCS2010NX/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TCS2203.Data;
+using TCS2203.Models;
+
+[assembly: HostingStartup(typeof(TCS2203.Areas.Identity.IdentityHostingStartup))]
+namespace TCS2203.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
